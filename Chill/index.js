e8afe2df01faba1ab8d8f73325f86f3d4a87649c
FaonDev <124811747+FaonDev@users.noreply.github.com>
2023-02-24 02:39:55 -0300

    Delete index.js

--- Chill/index.js
+++
@@ -1,5 +0,0 @@
-var Rest = [];
-class Chill { constructor(a, b) { this.item = a, this.delay = b } set(a, b) { Rest.push(a), setTimeout(() => Rest.splice(Rest.indexOf(a), 1), b) } get(a) { return Rest.includes(a) } };
-
-exports.Chill = Chill;
-// ==> Código fonte livre de direitos autoriais. Contudo, caso realize alterações bruscas, considere menções.
